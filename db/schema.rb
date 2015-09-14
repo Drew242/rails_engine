114a71d12221cf30e11452f73b41d2d0ee269ef7
Drew Conly <atconly@gmail.com>
2015-09-14 14:28:55 -0600

    add models: transaction, invoice, item, invoice_item, customer

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,62 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150914200756) do
+ActiveRecord::Schema.define(version: 20150914202749) do
+
+  create_table "customers", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "invoice_items", force: :cascade do |t|
+    t.integer  "item_id"
+    t.string   "invoice_references"
+    t.integer  "quantity"
+    t.decimal  "unit_price"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
+
+  add_index "invoice_items", ["item_id"], name: "index_invoice_items_on_item_id"
+
+  create_table "invoices", force: :cascade do |t|
+    t.integer  "customer_id"
+    t.integer  "merchant_id"
+    t.string   "status"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "invoices", ["customer_id"], name: "index_invoices_on_customer_id"
+  add_index "invoices", ["merchant_id"], name: "index_invoices_on_merchant_id"
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.decimal  "unit_price"
+    t.integer  "merchant_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "items", ["merchant_id"], name: "index_items_on_merchant_id"
 
   create_table "merchants", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "transactions", force: :cascade do |t|
+    t.integer  "invoice_id"
+    t.string   "credit_card_number"
+    t.string   "result"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
+
+  add_index "transactions", ["invoice_id"], name: "index_transactions_on_invoice_id"
 
 end
