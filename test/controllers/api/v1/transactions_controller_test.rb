8d7f14228a50d94ea0d62a848a94adfa733194ef
Drew Conly <atconly@gmail.com>
2015-09-15 14:21:30 -0600

    make all controller tests pass

---
+++ test/controllers/api/v1/transactions_controller_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class Api::V1::TransactionsControllerTest < ActionController::TestCase
+  test "#index" do
+    get :index, format: :json
+
+    transactions   = JSON.parse(response.body)
+    transaction    = transactions.first
+
+    assert_response :success
+    assert_equal 3, transactions.count
+    assert 3,       transaction["transaction_id"]
+  end
+
+  test "#show" do
+    get :show, format: :json, id: Transaction.first.id
+
+    transaction = JSON.parse(response.body, symbolize_names: true)
+
+    assert_response :success
+    assert_equal 3,                   transaction[:invoice_id]
+    assert_equal "4232569591693416",  transaction[:credit_card_number]
+    assert_equal "success",           transaction[:result]
+  end
+end
